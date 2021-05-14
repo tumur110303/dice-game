47abccd74e9ddef933123b0375b9fd7bf257a943
tumuruu <mbt.isee@gmail.com>
2021-05-14 18:59:48 +0800

    shoog shiddeg bolgoson

--- app.js
+++ app.js
@@ -7,18 +7,29 @@ var scores = [0, 0];
 // Тоглогч тоглож байх үеийн ээлжийн оноог хадгалах хувьсагч.
 var roundScore = 0;
 // Шооны аль талаараа буусны хадгалах хувьсагч хэрэгтэй. 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#score-0").textContent = 0; //document.querySelector - р HTML дээр бичигдсэн class-ыг # тэмдэглэгээ ашиглан дуудлаа.
-document.querySelector("#score-1").textContent = 0;
+/* document.querySelector("#score-0").textContent = 0; //document.querySelector - р HTML дээр бичигдсэн class-ыг # тэмдэглэгээ ашиглан ID-аар нь хайж, дуудлаа.
+document.querySelector("#score-1").textContent = 0;  //Энд # тэмдэглэгээ тавигдсан учраас ID-аар нь хайдаг.
 
 document.querySelector("#current-0").textContent = 0;
 document.querySelector("#current-1").textContent = 0;
+*/
+
+document.getElementById("score-0").textContent = 0; //Тусгай # тэмдэглэгээ тавих шаардлагагүй. Зөвхөн ID-р хайлт хийхэд ашигладаг.
+document.getElementById("score-1").textContent = 0; //querySelector-с давуу тал нь зөвхөн ID-р хайдаг учраас хурдан ажилладаг.
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
 //class-аар шооны зургийг хайх.
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
 
-console.log("Шоо " + dice);
+diceDom.style.display = "none"; // "none" - HTML дотор байсан зургийг байхгүй болгож харагдуулна.
 
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block"; //"block" - HTML дотор байсан зургийг харагдуулна.
+  diceDom.src = "dice-" + diceNumber + ".png"; //src рүү нь хандаж байна. HTML үз. Ж нь: png файлууд dice-4.png гэсэн нэртэй байгааг дуудаж ажиллууллаа.
+}); //addEventListener доторх "click" үйлдэл нь яг ямар үйлдэл хийхийг зааж өглөө.
 // #### №41. ПРОГРАМ АЖИЛЛАХ БЭЛТГЭЛ АЖИЛ (ТӨГСГӨЛ.) ####
 
 /* #### №42.  JAVASCRIPT-ЫН EVENT ГЭЖ ЮУ ВЭ? ЭВЕНТ НЬ БИДНИЙ ВЭБ ХУУДАС ДЭЭР БОЛЖ БАЙГАА ҮЙЛ ЯВЦ, ПРОЦЕСС ЮМ. 
@@ -32,3 +43,5 @@ console.log("Шоо " + dice);
  3. click хийхэд 1-6 хооронд санамсаргүй тоо үүсгээд харгалзах шооны зургийг DOM-оос авна. Уг обьектийг вэб дээр харагддаг болгоно.
     Уг обьектийн зургийг  өөрчилнө.
  */
+
+// #### №43. Тоглогчийн ээлжийг солих. ####
